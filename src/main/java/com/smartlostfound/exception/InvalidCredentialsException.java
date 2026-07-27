36f10d1880134484720dcec95089fbf1b53c44d4
Anshika-Sharma326 <anshikasharma0914@gmail.com>
2026-07-27 09:06:20 +0530

    Initial commit

---
+++ src/main/java/com/smartlostfound/exception/InvalidCredentialsException.java
@@ -0,0 +1,9 @@
+package com.smartlostfound.exception;
+
+public class InvalidCredentialsException extends RuntimeException {
+
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/test/java/com/smartlostfound/SmartLostFoundBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartlostfound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartLostFoundBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
