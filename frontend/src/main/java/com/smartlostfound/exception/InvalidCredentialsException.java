084ffbae84b02edb1f87ece9663578927521d741
Anshika-Sharma326 <anshikasharma0914@gmail.com>
2026-09-13 21:07:01 +0530

    Clean up frontend structure

--- frontend/src/main/java/com/smartlostfound/exception/InvalidCredentialsException.java
+++
@@ -1,9 +0,0 @@
-package com.smartlostfound.exception;
-
-public class InvalidCredentialsException extends RuntimeException {
-
-    public InvalidCredentialsException(String message) {
-        super(message);
-    }
-
-}
